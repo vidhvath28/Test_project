0b2566fa286a0c15e949d3acf53d18559004c5a2
vidhvath28 <vidhvathj.2005@gmail.com>
2024-04-15 11:28:21 +0530

    Day02

---
+++ user.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+let UserData = mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+let User = mongoose.model('data', UserData);
+
+export default User;
